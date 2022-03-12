a1a05e293a60b0ae580cdf30b8a766253ff5428d
MPA-PC\MPA <uglyfreaky@gmail.com>
2022-03-12 23:26:24 +0100

    Les alertes finies mais c'est bien moche (le style comme le code)

--- invert.js
+++ invert.js
@@ -50,46 +50,59 @@
 	// TODO - messages popup selon longueur texte ? Ou si un certain texte (ex: "Corentin") est trouvé dans le champ txt
 
 	//Fonction temps réel
-	const text1direct = document.getElementById("normaldirect").value;
-	const text2direct = document.getElementById("inverseddirect").value;
+const text1direct = document.getElementById("normaldirect").value;
+const text2direct = document.getElementById("inverseddirect").value;
 
 	function directReverse (){
 		const activeTextarea = document.activeElement.id;
+		const regexCorentin = new RegExp("Corentin", 'i');
 		console.log(activeTextarea);
 		
 		if (activeTextarea == "normaldirect") {
-		let textField = document.form2.texte1direct.value;
+		var textField = document.form2.texte1direct.value;
 		//console.log(textField);
 		document.form2.texte2direct.value = textField.split("").reverse().join("");
 		}
 		else if (activeTextarea == "inverseddirect") {
-		let textField2 = document.form2.texte2direct.value;
+		var textField2 = document.form2.texte2direct.value;
 		//console.log(textField2);
 		document.form2.texte1direct.value = textField2.split("").reverse().join("");
 		}
 
+	//La partie regex qui va chercher "Corentin" et faire pop la div d'alerte
+		if (regexCorentin.test(textField) == true){
+			document.getElementById('alertcoco').style.display = 'flex';
+		}
+		else if (regexCorentin.test(textField) == false) {
+			document.getElementById('alertcoco').style.display = 'none';
+		}
+
+	//La partie pour les warnings
+		var longueurTexte = 0;
+		longueurTexte = textField.length;
+		if (longueurTexte >= 20){
+			document.getElementById('warning1').style.display = 'flex';
+			document.getElementById('warning2').style.display = 'none';
+		}
+		if (longueurTexte < 20) {
+			document.getElementById('warning1').style.display = 'none';
+		}
+		if (longueurTexte >= 30){
+			document.getElementById('warning2').style.display = 'flex';
+			document.getElementById('warning1').style.display = 'none';
+		}
+
+
 		return null;
 	}
 
+	//Appels des fonctions
 	document.getElementById("normaldirect").onkeyup = directReverse;
 	document.getElementById("inverseddirect").onkeyup = directReverse;
 	//document.getElementsByClassName("textareadirect").onkeyup = directReverse;
+	//document.getElementById("normaldirect").onkeyup = alertDivs;
 
 	document.getElementById("directlog").onclick = function() {
 		console.log(textField + " - " + textField2);
 		}
-
-
-	//Fonction qui fait poper les alertes
-	function alertDivs(){
-		let text1 = document.getElementById("normaldirect").value;
-		let text2 = document.getElementById("inverseddirect").value;
-		
-		let regexCorentin = new RegExp("Corentin", 'i');
-		if (textField.match(regexCorentin)){
-			document.getElementById('alertcoco').style.display = 'none';
-			console.log("ici");
-		}
-	}
-
 document.write(Date());
